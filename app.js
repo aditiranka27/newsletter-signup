e921bf127da4cdb663b9fa0c3bd292cd945ddaa6
aditiranka27 <Aditi@Phoenix.local>
2021-05-25 21:49:17 -0500

    First Commit

---
+++ app.js
@@ -0,0 +1,83 @@
+const express = require("express");
+const https = require("https");
+const { url } = require("inspector");
+
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({extended:true}));
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname+"/signup.html")
+})
+
+app.post("/", function(req,res){
+    const firstName = req.body.fName;
+    const lastName= req.body.lName;
+    const email= req.body.email;
+    
+    const data= {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName 
+                }
+            }
+        ]
+    };
+    const jsonData= JSON.stringify(data);
+
+    const url = "https://us6.api.mailchimp.com/3.0/lists/01bc489952";
+
+    const options = {
+        method: "POST",
+        auth: "aditi27:ef44b7d92ebe5585ea3caed51eb84640-us6"
+    };
+
+
+    const request= https.request(url,options,function(response){
+        if(response.statusCode===200){
+            res.sendFile(__dirname+"/success.html")
+        }else{
+            res.sendFile(__dirname+"/failure.html")
+        }
+        
+        
+        
+        response.on("data", function(data){
+            console.log(JSON.parse(data));
+        });
+
+    });
+
+    request.write(jsonData);
+    request.end();
+});
+
+app.post("/failure",function(req,res){
+    res.redirect("/");
+})
+
+app.listen(process.env.PORT||3000,function(){
+    console.log("Server is running on port 3000");
+})
+
+
+
+
+
+
+
+
+
+
+// API Key
+// ef44b7d92ebe5585ea3caed51eb84640-us6
+
+// List ID
+// 01bc489952
